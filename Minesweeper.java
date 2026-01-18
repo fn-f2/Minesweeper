b9efc6609e6a2b081d25747a4852404aa6c6798b
carter <89955530+fn-f2@users.noreply.github.com>
2026-01-17 23:47:51 -0800

    Add files via upload

--- Minesweeper.java
+++ Minesweeper.java
@@ -7,11 +7,7 @@ public class Minesweeper
     final static int MINE = -1;
     final static int EMPTY = 0;
 
-    final static int EASY = 10;
-    final static int MEDIUM = 20;
-    final static int HARD = 40;
-
-    static int gridwidth;
+    static int gridwidth = 10;
     static int[][] grid;
     static boolean[][] revealed;
 
@@ -20,31 +16,58 @@ public class Minesweeper
         System.out.print("\f");
 
         //initialize stuff
-        gridwidth = EASY;
         grid = new int[gridwidth][gridwidth];
         revealed = new boolean[gridwidth][gridwidth];
         Scanner in = new Scanner(System.in);
         nummines = gridwidth*gridwidth*15/100;
-
-        int input = in.nextInt();
-        initializeGrid(input/10, input%10);
+        printGrid();
+        
+        String input = in.nextLine();
+        
+        String[] inps = input.split(",");
+        
+        int y = Integer.parseInt(inps[0]);
+        int x = Integer.parseInt(inps[1]);
+        
+        initializeGrid(y, x);
 
         //game loop
         do
         {
-            System.out.println("\f");
-            if (grid[input/10][input%10] == 0) floodClear(input/10, input%10);
-            else if (grid[input/10][input%10] > 0) revealed[input/10][input%10] = true;
+            System.out.print("\f");
+            if (grid[y][x] == 0) floodClear(y, x);
+            else if (grid[y][x] > 0) revealed[y][x] = true;
             printGrid();
 
-            input = in.nextInt();
-        } while (true);
+            input = in.nextLine();
+            inps = input.split(",");
+        
+
+            ;
+            x = Integer.parseInt(inps[1]);
+        } while (!hasWon());
     }
 
+    public static boolean hasWon()
+    {
+        for (int r = 0; r < gridwidth; r++)
+        {
+            for (int c = 0; c < gridwidth; c++)
+            {
+                if (grid[r][c] != MINE && !revealed[r][c])return false;
+            }
+        }
+        
+        System.out.print("\f");
+        System.out.print("UYOU WIN YEAH WOOOO YEAH YEAH");
+        return true;
+    }
+    
     public static void floodClear(int x, int y)
     {
-        if (grid[x][y] == 0 && x  < gridwidth && x > -1 && y < gridwidth && y > -1)
+        if (x  < gridwidth && x > -1 && y < gridwidth && y > -1 && grid[x][y] == 0 && !revealed[x][y])
         {
+            revealed[x][y] = true;
             floodClear(x+1, y);
             floodClear(x-1, y);
             floodClear(x, y+1);
@@ -60,108 +83,106 @@ public class Minesweeper
             }
         }
     }
-    return;
-}
 
-public static void printGrid()
-{
-//coords and upper/side border
-System.out.print("   ");
-for (int i = 0; i < gridwidth; i++)
-{
-System.out.print("(");
-System.out.print(i + ")");
-}
-System.out.print("\n");
-/*
-for (int i = 0; i < gridwidth; i++)
-{
-System.out.print("▁▁▁");
-}
-System.out.println();
- */
+    public static void printGrid()
+    {
+        //coords and upper/side border
+        System.out.print("     ");
+        for (int i = 0; i < gridwidth; i++)
+        {
+            System.out.print("(");
+            System.out.print(i + ")");
+        }
+        System.out.print("\n     ");
+        
+        for (int i = 0; i < gridwidth; i++)
+        {
+        System.out.print("▁▁▁");
+        }
+        System.out.println();
+        
 
-//print grid
-boolean dark = false;
-for (int r = 0; r < gridwidth; r++)
-{
-System.out.print("(");
-System.out.print(r + ")"); //▕
-for (int c = 0; c < gridwidth; c++)
-{
-if (revealed[r][c])
-{
-if (grid[r][c] != MINE)
-{
-System.out.print(" ");
-System.out.print(grid[r][c] + " ");
-} else System.out.print(" M "); //priunt MINE :0
-}
-else if (dark) System.out.print("[▁]"); //◀
-else System.out.print("[▁]");
-dark = !dark;
-}
-System.out.println(""); //▏new line
-dark = !dark;
-}
+        //print grid
+        boolean dark = false;
+        for (int r = 0; r < gridwidth; r++)
+        {
+            System.out.print("(");
+            System.out.print(r + ") ▕");
+            for (int c = 0; c < gridwidth; c++)
+            {
+                if (revealed[r][c])
+                {
+                    if (grid[r][c] != MINE)
+                    {
+                        System.out.print(" ");
+                        System.out.print(grid[r][c] + " ");
+                    } else System.out.print(" M "); //priunt MINE :0
+                }
+                else if (dark) System.out.print("▓▓▓"); //◀
+                else System.out.print("▒▒▒");
+                dark = !dark;
+            }
+            System.out.println("▏"); //new line
+            dark = !dark;
+        }
 
-/*//bottom border
-System.out.print("    ");
-for (int i = 0; i < gridwidth; i++)
-{
-System.out.print("▔▔▔");
-}*/
-}
+        //bottom border
+        System.out.print("     ");
+        for (int i = 0; i < gridwidth; i++)
+        {
+        System.out.print("▔▔▔");
+        }
+    }
 
-public static void initializeGrid(int movex, int movey)
-{
-placeMines(movex, movey);
-for (int r = 0; r < gridwidth; r++)
-{
-for (int c = 0; c < gridwidth; c++)
-{
-revealed[r][c] = false;
-if (grid[r][c] != MINE) grid[r][c] = countSurrounding(r, c);
-}
-}
+    public static void initializeGrid(int movex, int movey)
+    {
+        placeMines(movex, movey);
+        for (int r = 0; r < gridwidth; r++)
+        {
+            for (int c = 0; c < gridwidth; c++)
+            {
+                revealed[r][c] = false;
+                if (grid[r][c] != MINE) grid[r][c] = countSurrounding(r, c);
+            }
+        }
 
-}
+    }
 
-public static void placeMines(int movex, int movey)
-{
-System.out.println("first move");
-while (nummines > 0)
-{
-int randomx = (int) (Math.random() * gridwidth);
-int randomy = (int) (Math.random() * gridwidth);
-while (grid[randomx][randomy] == MINE
-|| (randomx > movex-2 && randomx < movex+2
-&& randomy > movey-2 && randomy < movey+2))
-{
-randomx = (int) (Math.random() * gridwidth);
-randomy = (int) (Math.random() * gridwidth);
-}
-grid[randomx][randomy] = MINE;
+    public static void placeMines(int movex, int movey)
+    {
+        System.out.println("first move");
+        while (nummines > 0)
+        {
+            int randomx = (int) (Math.random() * gridwidth);
+            int randomy = (int) (Math.random() * gridwidth);
+            while (grid[randomx][randomy] == MINE
+            || (randomx > movex-2 && randomx < movex+2
+                && randomy > movey-2 && randomy < movey+2))
+            {
+                randomx = (int) (Math.random() * gridwidth);
+                randomy = (int) (Math.random() * gridwidth);
+            }
+            grid[randomx][randomy] = MINE;
 
-nummines--;
-}
-}
+            nummines--;
+        }
+    }
 
-public static int countSurrounding(int r, int c)
-{
-int count = 0;
+    public static int countSurrounding(int r, int c)
+    {
+        int count = 0;
 
-//loop thru surrounding squares
-for (int r2 = r-1; r2 < r+2; r2++)
-{
-for (int c2 = c-1; c2 < c+2; c2++)
-{
-if (r2 > -1 && c2 > -1 && r2 < gridwidth && c2 < gridwidth //ensure within bounds of board
-&& grid[r2][c2] == MINE) count++; //incremenbt surrounding mine count
-}
-}
+        //loop thru surrounding squares
+        for (int r2 = r-1; r2 < r+2; r2++)
+        {
+            for (int c2 = c-1; c2 < c+2; c2++)
+            {
+                if (r2 > -1 && c2 > -1 && r2 < gridwidth && c2 < gridwidth //ensure within bounds of board
+                && grid[r2][c2] == MINE) count++; //incremenbt surrounding mine count
+            }
+        }
 
-return count;        
-}
+        return count;        
+    }
 }
 
